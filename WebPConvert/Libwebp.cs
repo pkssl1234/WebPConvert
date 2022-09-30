291e40fc78dc0373454effa1b94e9f87f3628f1b
Randolph.chou <Randolph.chou@yotor.com.tw>
2022-09-30 09:56:08 +0800

    加入專案檔案。

---
+++ WebPConvert/Libwebp.cs
@@ -0,0 +1,35 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+
+namespace WebPConvert
+{
+#pragma warning disable CA1416 // 驗證平台相容性
+    public sealed class Libwebp
+    {
+        [DllImport("library/libwebp.dll", CallingConvention = CallingConvention.Cdecl)]
+        static extern int WebPEncodeBGRA(IntPtr rgba, int width, int height, int stride, float quality_factor, out IntPtr output);
+
+        [DllImport("library/libwebp.dll", CallingConvention = CallingConvention.Cdecl)]
+        static extern int WebPFree(IntPtr p);
+
+        public static void Encode()
+        {
+            using var source = new Bitmap("input.png");
+            var data = source.LockBits(
+                new Rectangle(0, 0, source.Width, source.Height),
+                ImageLockMode.ReadOnly,
+                PixelFormat.Format32bppArgb);
+            IntPtr webp_data;
+            int size = WebPEncodeBGRA(data.Scan0, source.Width, source.Height, data.Stride, 100, out webp_data);
+
+            byte[] buffer = new byte[size];
+            Marshal.Copy(webp_data, buffer, 0, size);
+            source.UnlockBits(data);
+            WebPFree(webp_data);
+
+            File.WriteAllBytes("encode.webp",buffer);
+        }
+    }
+#pragma warning restore CA1416 // 驗證平台相容性
+}
---
+++ WebPConvertTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using WebPConvert;
+namespace WebPConvertTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Libwebp.Encode();
+        }
+    }
+}
